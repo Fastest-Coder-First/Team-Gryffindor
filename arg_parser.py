95182197d3bccca72e720682b35536ef04fd406c
sinking8 <ashwinblaze111@gmail.com>
2023-06-24 13:33:08 +0530

    added

--- arg_parser.py
+++ arg_parser.py
@@ -1,20 +1,20 @@
 import argparse
+import json
+
+import os
 
 # Parser instance
 parser = argparse.ArgumentParser(
     description='Welcome to Python Weather CLI.\n This is a command line application that helps you retrieve weather information for a particular city.')
 
+# Load json file
+with open(os.path.join("configs", "config.json")) as f:
+    args_json = json.load(f)
 
+# Adding Arguments
 parser.add_argument("-helpers", action="store_true",
                     help="Display Commands")
 
-parser.add_argument('-lat', help='latitude', default=10.0)
-parser.add_argument('-long', help='longitude', default=10.0)
-parser.add_argument('-city', help='city')
-parser.add_argument('-country', help='country')
-parser.add_argument('-lang', help='language')
-parser.add_argument('-units', help='units')
-parser.add_argument("--to-csv", action="store_true",
-                    help="converts weather data to csv format")
-parser.add_argument('--temp-graph', action="store_true",
-                    help="Display Temperature Graph")
+for key, value in args_json['args'].items():
+    parser.add_argument(
+        key, help=value, default=10.0 if key in args_json['numeric_defaults'] else None, action="store_true" if key in args_json['bool_args'] else None)
