486587c96d0cc60825c1a01d40c67375138a937d
sinking8 <ashwinblaze111@gmail.com>
2023-06-24 11:17:39 +0530

    updated

--- weather.py
+++ weather.py
@@ -1,2 +1,20 @@
+import json
+import requests
+
+
 class Weather:
+    API_URL = "https://api.openweathermap.org/data/3.0/onecall?"
+
     def __init__(self): pass
+
+    def get_weather_data(self, params):
+        # defining a params dict for the parameters to be sent to the API
+        PARAMS = params
+
+        # sending get request and saving the response as response object
+        r = requests.get(url=self.API_URL, params=PARAMS)
+
+        # extracting data in json format
+        data = r.json()
+
+        print(data)
